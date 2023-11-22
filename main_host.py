64d9b20519feb7c57e78e1a006dd470c3d6e8e2c
Rahul-s-007 <rahulrtg7703@gmail.com>
2023-11-23 00:50:49 +0530

    new host file with dynamic selectbox

--- main_host.py
+++ main_host.py
@@ -1,10 +1,10 @@
 import streamlit as st
+from streamlit_extras.stateful_button import button 
+
 import firebase_admin
 from firebase_admin import credentials, db
-# import json
-# import os
-# from dotenv import load_dotenv
-# load_dotenv()
+
+import json
 
 DB_URL = st.secrets["DB_URL"]
 
@@ -30,41 +30,15 @@ def initialize_firebase():
     if not firebase_admin._apps:
         cred = credentials.Certificate(FIREBASE_JSON)
         firebase_admin.initialize_app(cred, {'databaseURL': DB_URL})
+        
+@st.cache_resource
+def get_location_details():
+    with open("pincode_db.json", "r") as f:
+        location_details = json.load(f)
+    return location_details
 
-indian_states = [
-    'Bihar',
-    'Maharashtra',
-    'Goa',
-    'Andhra Pradesh',
-    'Chhattisgarh',
-    'Karnataka',
-    'Mizoram',
-    'Punjab',
-    'Tripura',
-    'West Bengal',
-    'Arunachal Pradesh',
-    'Gujarat',
-    'Assam',
-    'Manipur',
-    'Haryana',
-    'Himachal Pradesh',
-    'Kerala',
-    'Jharkhand',
-    'Delhi',
-    'Rajasthan',
-    'Madhya Pradesh',
-    'Tamil Nadu',
-    'Odisha',
-    'Meghalaya',
-    'Telangana',
-    'Nagaland',
-    'Sikkim',
-    'Uttar Pradesh',
-    'Uttarakhand',
-    'Jammu & Kashmir'
-]
 
-aov_monthly_ranges = ["0-5 lakhs", "5-15 lakhs", "15-50 lakhs", "50 lakhs - 1 Cr", "1+ Cr"]
+aov_monthly_ranges = ["0-1 lakhs", "1-5 lakhs", "5-15 lakhs", "15-30 lakhs", "30-50 lakhs", "50 lakhs - 1 Cr", "1+ Cr"]
 
 #--------------------------------------------------------------------------------------------------------------------
 def store_details(form_type, details):
@@ -75,168 +49,240 @@ def store_details(form_type, details):
         print("DB error")
 
 #--------------------------------------------------------------------------------------------------------------------
-def retailer_form():
+def retailer_form(loc_details):
     st.title("Retailer Registration Form")
-    
-    with st.form("Form", clear_on_submit=False):
-        name_val = st.text_input("Full name:")
-        
-        col1, col2 = st.columns((1,1))
-        with col1:
-            phone_val = st.number_input("Phone number (+91):", min_value=1000000000, max_value=9999999999, value=1111111111, step=1)
-        with col2:
-            email_val = st.text_input("Email Address:")
 
-        address_val = st.text_input("Address:")
-
-        col1, col2 = st.columns((1,1))
-        with col1:
-            state_val = st.selectbox(label="State (India):", options=indian_states)
-        with col2:
-            pincode_val = st.number_input("Pincode:", min_value=999, max_value=999999999, value=111111, step=1)
+    name_val = st.text_input("Full name:")
     
-        drug_license_number_val = st.text_input("Drug Lisense Number:")
+    col1, col2 = st.columns((1,1))
+    with col1:
+        phone_val = st.number_input("Phone number (+91):", min_value=1000000000, max_value=9999999999, value=1111111111, step=1)
+    with col2:
+        email_val = st.text_input("Email Address:")
+
+    address_val = st.text_input("Address:")
+
+    col1, col2 = st.columns((5,1))
+    with col1:
+        state_val = st.selectbox(label="State (India):", options=loc_details.keys())
         
-        avg_order_value_monthly_val = st.selectbox("Average Monthly Order Value (INR):", aov_monthly_ranges)
-        
-        submitted = st.form_submit_button("Submit")
-        if submitted:
-            if name_val and phone_val and email_val and address_val and state_val and pincode_val and drug_license_number_val and avg_order_value_monthly_val:
-                try:
-                    form_type = "retailer"
+    with col2:
+        submit_state = button("Submit State", key="Submit State")
+    
+    if submit_state:
+        col1, col2 = st.columns((5,1))
+        with col1:
+            district_val = st.selectbox(label="District:", options=loc_details[state_val].keys())
+        with col2:
+            submit_district = button("Submit District", key="Submit District")
+    
+        if submit_district:
+            col1, col2 = st.columns((5,1))
+            with col1:
+                pincode_val = st.selectbox(label="Pincode:", options=loc_details[state_val][district_val].keys())
+            with col2:
+                submit_pincode = button("Submit Pincode", key="Submit Pincode")
+            
+            if submit_pincode:
+                col1, col2 = st.columns((5,1))
+                with col1:
+                    area_val = st.selectbox(label="Area:", options=loc_details[state_val][district_val][pincode_val])
+                with col2:
+                    submit_area = button("Submit Area", key="Submit Area")
+                
+                if submit_area:
+                    drug_license_number_val = st.text_input("Drug License Number:")
                     
-                    details = {
-                        "name": name_val,
-                        "phone": phone_val,
-                        "email": email_val,
-                        "address": address_val,
-                        "state": state_val,
-                        "pincode": pincode_val,
-                        "drug_license_number": drug_license_number_val,
-                        "avg_order_value_monthly": avg_order_value_monthly_val
-                    }
+                    avg_order_value_monthly_val = st.selectbox("Average Monthly Order Value (INR):", aov_monthly_ranges)
                     
-                    store_details(form_type, details)
-                    
-                    st.success("Form Submitted Sucessfully")
-                except:
-                    st.error("Error in Submitting the Form")
-            else:
-                st.error("All fields are mandatory. Please fill them.")
+                    submitted = st.button("Submit Form")
+                    if submitted:
+                        if name_val and phone_val and email_val and address_val and state_val and pincode_val and drug_license_number_val and avg_order_value_monthly_val:
+                            try:
+                                form_type = "retailer"
+                                
+                                details = {
+                                    "name": name_val,
+                                    "phone": phone_val,
+                                    "email": email_val,
+                                    "address": address_val,
+                                    "state": state_val,
+                                    "pincode": pincode_val,
+                                    "drug_license_number": drug_license_number_val,
+                                    "avg_order_value_monthly": avg_order_value_monthly_val
+                                }
+                                
+                                store_details(form_type, details)
+                                
+                                st.success("Form Submitted Sucessfully")
+                            except:
+                                st.error("Error in Submitting the Form")
+                        else:
+                            st.error("All fields are mandatory. Please fill them.")
+
 
 #--------------------------------------------------------------------------------------------------------------------
-def hospital_form():
+def hospital_form(loc_details):
     st.title("Hospital Registration Form")
     
-    with st.form("Form", clear_on_submit=False):
-        name_val = st.text_input("Full name:")
-        
-        col1, col2 = st.columns((1,1))
-        with col1:
-            phone_val = st.number_input("Phone number (+91):", min_value=1000000000, max_value=9999999999, value=1111111111, step=1)
-        with col2:
-            email_val = st.text_input("Email Address:")
-
-        address_val = st.text_input("Address:")
-
-        col1, col2 = st.columns((1,1))
-        with col1:
-            state_val = st.selectbox(label="State (India):", options=indian_states)
-        with col2:
-            pincode_val = st.number_input("Pincode:", min_value=999, max_value=999999999, value=111111, step=1)
+    name_val = st.text_input("Full name:")
     
-        drug_license_number_val = st.text_input("Drug Lisense Number:")
+    col1, col2 = st.columns((1,1))
+    with col1:
+        phone_val = st.number_input("Phone number (+91):", min_value=1000000000, max_value=9999999999, value=1111111111, step=1)
+    with col2:
+        email_val = st.text_input("Email Address:")
+
+    address_val = st.text_input("Address:")
+
+    col1, col2 = st.columns((5,1))
+    with col1:
+        state_val = st.selectbox(label="State (India):", options=loc_details.keys())
         
-        operating_under_pharmacist_name_val = st.text_input("Operating Under Pharmacist name:")
-        
-        avg_order_value_monthly_val = st.selectbox("Average Monthly Order Value (INR):", aov_monthly_ranges)
-        
-        submitted = st.form_submit_button("Submit")
-        if submitted:
-            if name_val and phone_val and email_val and address_val and state_val and pincode_val and drug_license_number_val and avg_order_value_monthly_val:
-                try:
-                    form_type = "hospital"
+    with col2:
+        submit_state = button("Submit State", key="Submit State")
+    
+    if submit_state:
+        col1, col2 = st.columns((5,1))
+        with col1:
+            district_val = st.selectbox(label="District:", options=loc_details[state_val].keys())
+        with col2:
+            submit_district = button("Submit District", key="Submit District")
+    
+        if submit_district:
+            col1, col2 = st.columns((5,1))
+            with col1:
+                pincode_val = st.selectbox(label="Pincode:", options=loc_details[state_val][district_val].keys())
+            with col2:
+                submit_pincode = button("Submit Pincode", key="Submit Pincode")
+            
+            if submit_pincode:
+                col1, col2 = st.columns((5,1))
+                with col1:
+                    area_val = st.selectbox(label="Area:", options=loc_details[state_val][district_val][pincode_val])
+                with col2:
+                    submit_area = button("Submit Area", key="Submit Area")
+                
+                if submit_area:
+                    drug_license_number_val = st.text_input("Drug License Number:")
                     
-                    details = {
-                        "name": name_val,
-                        "phone": phone_val,
-                        "email": email_val,
-                        "address": address_val,
-                        "state": state_val,
-                        "pincode": pincode_val,
-                        "drug_license_number": drug_license_number_val,
-                        "operating_under_pharmacist_name": operating_under_pharmacist_name_val,
-                        "avg_order_value_monthly": avg_order_value_monthly_val
-                    }
+                    operating_under_pharmacist_name_val = st.text_input("Operating Under Pharmacist name:")
                     
-                    store_details(form_type, details)
+                    avg_order_value_monthly_val = st.selectbox("Average Monthly Order Value (INR):", aov_monthly_ranges)
                     
-                    st.success("Form Submitted Sucessfully")
-                except:
-                    st.error("Error in Submitting the Form")
-            else:
-                st.error("All fields are mandatory. Please fill them.")
+                    submitted = st.button("Submit Form")
+                    if submitted:
+                        if name_val and phone_val and email_val and address_val and state_val and pincode_val and drug_license_number_val and avg_order_value_monthly_val:
+                            try:
+                                form_type = "hospital"
+                                
+                                details = {
+                                    "name": name_val,
+                                    "phone": phone_val,
+                                    "email": email_val,
+                                    "address": address_val,
+                                    "state": state_val,
+                                    "pincode": pincode_val,
+                                    "drug_license_number": drug_license_number_val,
+                                    "operating_under_pharmacist_name": operating_under_pharmacist_name_val,
+                                    "avg_order_value_monthly": avg_order_value_monthly_val
+                                }
+                                
+                                store_details(form_type, details)
+                                
+                                st.success("Form Submitted Sucessfully")
+                            except:
+                                st.error("Error in Submitting the Form")
+                        else:
+                            st.error("All fields are mandatory. Please fill them.")
 
 #--------------------------------------------------------------------------------------------------------------------
-def pharmacist_form():
+def pharmacist_form(loc_details):
     st.title("Pharmacist Registration Form")
 
-    with st.form("Form", clear_on_submit=False):
-        name_val = st.text_input("Full name:")
-        
-        col1, col2 = st.columns((1,1))
-        with col1:
-            phone_val = st.number_input("Phone number (+91):", min_value=1000000000, max_value=9999999999, value=1111111111, step=1)
-        with col2:
-            email_val = st.text_input("Email Address:")
-
-        address_val = st.text_input("Address:")
-
-        col1, col2 = st.columns((1,1))
-        with col1:
-            state_val = st.selectbox(label="State (India):", options=indian_states)
-        with col2:
-            pincode_val = st.number_input("Pincode:", min_value=999, max_value=999999999, value=111111, step=1)
+    name_val = st.text_input("Full name:")
     
-        drug_license_number_val = st.text_input("Drug Lisense Number:")
+    col1, col2 = st.columns((1,1))
+    with col1:
+        phone_val = st.number_input("Phone number (+91):", min_value=1000000000, max_value=9999999999, value=1111111111, step=1)
+    with col2:
+        email_val = st.text_input("Email Address:")
+
+    address_val = st.text_input("Address:")
+
+    col1, col2 = st.columns((5,1))
+    with col1:
+        state_val = st.selectbox(label="State (India):", options=loc_details.keys())
         
-        submitted = st.form_submit_button("Submit")
-        if submitted:
-            if name_val and phone_val and email_val and address_val and state_val and pincode_val and drug_license_number_val:
-                try:
-                    form_type = "pharmacist"
+    with col2:
+        submit_state = button("Submit State", key="Submit State")
+    
+    if submit_state:
+        col1, col2 = st.columns((5,1))
+        with col1:
+            district_val = st.selectbox(label="District:", options=loc_details[state_val].keys())
+        with col2:
+            submit_district = button("Submit District", key="Submit District")
+    
+        if submit_district:
+            col1, col2 = st.columns((5,1))
+            with col1:
+                pincode_val = st.selectbox(label="Pincode:", options=loc_details[state_val][district_val].keys())
+            with col2:
+                submit_pincode = button("Submit Pincode", key="Submit Pincode")
+            
+            if submit_pincode:
+                col1, col2 = st.columns((5,1))
+                with col1:
+                    area_val = st.selectbox(label="Area:", options=loc_details[state_val][district_val][pincode_val])
+                with col2:
+                    submit_area = button("Submit Area", key="Submit Area")
+                
+                if submit_area:
+                    drug_license_number_val = st.text_input("Drug License Number:")
                     
-                    details = {
-                        "name": name_val,
-                        "phone": phone_val,
-                        "email": email_val,
-                        "address": address_val,
-                        "state": state_val,
-                        "pincode": pincode_val,
-                        "drug_license_number": drug_license_number_val,
-                    }
+                    avg_order_value_monthly_val = st.selectbox("Average Monthly Order Value (INR):", aov_monthly_ranges)
                     
-                    store_details(form_type, details)
-                    
-                    st.success("Form Submitted Sucessfully")
-                except:
-                    st.error("Error in Submitting the Form")
-            else:
-                st.error("All fields are mandatory. Please fill them.")
+                    submitted = st.button("Submit Form")
+                    if submitted:
+                        if name_val and phone_val and email_val and address_val and state_val and pincode_val and drug_license_number_val and avg_order_value_monthly_val:
+                            try:
+                                form_type = "retailer"
+                                
+                                details = {
+                                    "name": name_val,
+                                    "phone": phone_val,
+                                    "email": email_val,
+                                    "address": address_val,
+                                    "state": state_val,
+                                    "pincode": pincode_val,
+                                    "drug_license_number": drug_license_number_val,
+                                    "avg_order_value_monthly": avg_order_value_monthly_val
+                                }
+                                
+                                store_details(form_type, details)
+                                
+                                st.success("Form Submitted Sucessfully")
+                            except:
+                                st.error("Error in Submitting the Form")
+                        else:
+                            st.error("All fields are mandatory. Please fill them.")
 
 #--------------------------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
     initialize_firebase()
+    loc_details = get_location_details()
+
     st.sidebar.image("Genme-removebg-preview.png")
     st.sidebar.title("Welcome to Genme")
     
-    st.sidebar.subheader("Register with us as:")
+    st.sidebar.subheader("Register with us as a Partner:")
     option = st.sidebar.radio("Select:", ("Retailer", "Hospital", "Pharmacist"))
 
     # Perform actions based on user selection
     if option == "Retailer":
-        retailer_form()
+        retailer_form(loc_details)
     elif option == "Hospital":
-        hospital_form()
+        hospital_form(loc_details)
     elif option == "Pharmacist":
-        pharmacist_form()  
+        pharmacist_form(loc_details)  
